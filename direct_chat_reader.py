9240c4d4fbea4dc47a4b3e451a1aa930dbc30bf6
Lukas Hueller <lukas.hueller@student.hpi.uni-potsdam.de>
2022-06-21 18:12:43 +0200

    WIP: add messageTypes

--- direct_chat_reader.py
+++ direct_chat_reader.py
@@ -1,12 +1,24 @@
 import re
-#from pprint import pprint
+from enum import Enum
+
+from pprint import pprint
 
 class DirectChatReader():
+    class MessageTypes(Enum):
+        TEXT = 1
+        AUDIO = 2
+        IMAGE = 3
+        VIDEO = 4
+        GIF = 5
+        STICKER = 6
+        DOCUMENT = 7
+    
     class directChatObject(object):
-        def __init__(self, timestamp, person, text):
+        def __init__(self, timestamp, person, message, messageType):
             self.timestamp = timestamp
             self.person = person
-            self.text = text
+            self.message = message
+            self.message_type = messageType
     
     chat = []
 
@@ -20,29 +32,58 @@ class DirectChatReader():
         count = 0
         for line in Lines:
             #if count % 100 == 0 : print(count)
-            #if count == 10000 : break # JUST FOR DEBUGGING (Interrupt after 5 lines)
+            if count == 35 : break # JUST FOR DEBUGGING (Interrupt after 5 lines)
 
             self.extract(line)
             count += 1
 
-        if self.chat[0].text.startswith("Messages and calls are end-to-end encrypted.") : 
+        if self.chat[0].message.startswith("Messages and calls are end-to-end encrypted.") : 
             self.chat.pop(0)
 
-        #for line in self.chat : 
-        #    pprint(vars(line))
+        """ for line in self.chat : 
+            pprint(vars(line)) """
+
+        self.print_statistics()
 
         print(" --- END READ " + filename)
 
     def extract(self, line:str) : 
-        #print(re.split(']|:', line, 2))
+        # check if this line is a new message
         if re.match(r'^\[\d\d\.\d\d\.\d\d, \d\d:\d\d:\d\d\]', line) :
             result = re.search(r'^\[(.*?)\](.*?)\:(\s\S.*)', line)
             
-            singleChatLineObject = self.directChatObject(
-                result.group(1).strip().replace("\u200e",""),
-                result.group(2).strip().replace("\u200e",""),
-                result.group(3).strip().replace("\u200e","")
-            )
+            timestamp = result.group(1).strip().replace("\u200e","")
+            person = result.group(2).strip().replace("\u200e","")
+            message = result.group(3).strip().replace("\u200e","")
+            
+            message_type = self.return_message_type(message)
+
+            singleChatLineObject = self.directChatObject(timestamp, person, message, message_type)
             self.chat.append(singleChatLineObject)
         else :
-            self.chat[-1].text = self.chat[-1].text + "\n" + line
+            self.chat[-1].message = self.chat[-1].message + "\n" + line
+
+    def return_message_type(self, message:str) : 
+        if "audio omitted" in message : 
+            return self.MessageTypes.AUDIO
+        elif "image omitted" in message : 
+            return self.MessageTypes.IMAGE
+        elif "video omitted" in message : 
+            return self.MessageTypes.VIDEO
+        elif "GIF omitted" in message : 
+            return self.MessageTypes.GIF
+        elif "sticker omitted" in message : 
+            return self.MessageTypes.STICKER
+        elif "document omitted" in message : 
+            return self.MessageTypes.DOCUMENT
+        else :
+            return self.MessageTypes.TEXT
+
+    def print_statistics(self) : 
+        print(" ---- Number of Messages in Chat: " + str(len(self.chat)))
+        print(" ---- Number of TEXT MESSAGES send: " + str(sum(map(lambda x : x.message_type == self.MessageTypes.TEXT, self.chat))))
+        print(" ---- Number of IMAGES send: " + str(sum(map(lambda x : x.message_type == self.MessageTypes.IMAGE, self.chat))))
+        print(" ---- Number of GIFS send: " + str(sum(map(lambda x : x.message_type == self.MessageTypes.GIF, self.chat))))
+
+
+
