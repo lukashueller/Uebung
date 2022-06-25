303a5f20c1bca3b4aadcdc00c44153fa726a963f
Lukas Hueller <lukas.hueller@student.hpi.uni-potsdam.de>
2022-06-25 17:05:35 +0200

    add args

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ from py_classes.statistics_printer import StatisticsPrinter
 @click.command()
 @click.option("-d", "--direct", "direct_chat_filename", type=str, help="The name of the text file to analyze the one to one whatsapp-chat")
 @click.option("-g", "--group", "group_chat_filename", type=str, help="The name of the text file to analyze the group whatsapp-chat")
-@click.option("-e", "--emoji", "print_emojis", type=str, help="type \"all\" to get all emojis; otherwise only the 10 most common emojis will be displayed")
+@click.option("-e", "--emoji", "number_print_emojis", type=str, help="type \"all\" to get all emojis; otherwise only the requested number of most common emojis will be displayed (default: 10)")
+@click.option("-w", "--words", "number_print_words", type=str, help="type \"all\" to get all words; otherwise only the requested number of most words emojis will be displayed (default: 15)")
 
-def run(direct_chat_filename : str, group_chat_filename : str, print_emojis : str) :
+def run(direct_chat_filename : str, group_chat_filename : str, number_print_emojis : str, number_print_words : str) :
     print(" - WHATSAPP ANALYSER STARTED")
     if direct_chat_filename and group_chat_filename:
         print("You can only upload one chat at the moment.")
     elif direct_chat_filename :
         chat_data = DirectChatReader().read_chat(direct_chat_filename)
-        StatisticsPrinter().print_statistics(chat_data[0], chat_data[1], print_emojis)
+        StatisticsPrinter().print_statistics(chat_data[0], chat_data[1], number_print_emojis, number_print_words)
     elif group_chat_filename : 
         print("Group chat analysis is not supportet at the moment.")
     else : 
--- py_classes/statistics_printer.py
+++ py_classes/statistics_printer.py
@@ -4,10 +4,10 @@ from py_classes.models.MessageTypes import MessageTypes
 from sklearn.feature_extraction.text import CountVectorizer
 
 class StatisticsPrinter():
-    def print_statistics(self, preprocessed_chat, emojiArray, print_emojis) : 
+    def print_statistics(self, preprocessed_chat, emojiArray, number_print_emojis, number_print_words) : 
         self.print_message_type_statistics(preprocessed_chat)
-        self.print_emoji_statistics(emojiArray, print_emojis)
-        self.print_word_count(preprocessed_chat)
+        self.print_emoji_statistics(emojiArray, number_print_emojis)
+        self.print_word_count(preprocessed_chat, number_print_words)
 
     def print_message_type_statistics(self, preprocessed_chat) :
         print(" --- MESSAGE TYPE STATISTICS:")
@@ -20,16 +20,18 @@ class StatisticsPrinter():
         print(" ---- Number of STICKERS send: " + str(sum(map(lambda x : x.message_type == MessageTypes.STICKER, preprocessed_chat))))
         print(" ---- Number of DOCUMENTS send: " + str(sum(map(lambda x : x.message_type == MessageTypes.DOCUMENT, preprocessed_chat))))
         
-    def print_emoji_statistics(self, emojiArray, print_emojis) :
+    def print_emoji_statistics(self, emojiArray, number_print_emojis) :
         print(" --- EMOJI STATISTICS:")
-        print(("{:<8} {:>8}").format('  EMOJI','NUMBER'))
+        print(("{:<8} {:>8}").format('   EMOJI','NUMBER'))
         emojiCounter = Counter(emojiArray).most_common()
 
         for i, item in enumerate(emojiCounter) :
+            if number_print_emojis is None and i > 10: break   # default number of displayed emojis
+            if (number_print_emojis is not None) and (number_print_emojis != "all") and (int(number_print_emojis) == i) : break
             print(("{:<8} {:>5}").format("    " + item[0], item[1]))
-            if print_emojis != "all" and i == 10 : break 
 
-    def print_word_count(self, preprocessed_chat):
+    def print_word_count(self, preprocessed_chat, number_print_words):
+        print(" --- WORD COUNT STATISTICS:")
         ## Source: https://stackoverflow.com/questions/33404752/removing-emojis-from-a-string-in-python (modified)
         def give_emoji_free_text(text):
             return emoji.get_emoji_regexp().sub(r'', text)
@@ -44,7 +46,8 @@ class StatisticsPrinter():
 
         wordsCounter = Counter(messages_as_text.split()).most_common()
 
-        print(("{:<13} {:>10}").format('    WORD','       NUMBER'))
+        print(("{:<13} {:>10}").format('   WORD','     NUMBER'))
         for i, item in enumerate(wordsCounter) :
-            print(("{:<15} {:>10}").format("    " + item[0], item[1]))
-            if i == 20 : break 
+            if number_print_words is None and i > 15: break   # default number of displayed words
+            if (number_print_words is not None) and (number_print_words != "all") and (int(number_print_words) == i) : break
+            print(("{:<15} {:>8}").format("    " + item[0], item[1]))
