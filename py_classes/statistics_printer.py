8f26e9b7701e1774f59643da366245d0eb111bdb
Lukas Hueller <lukas.hueller@student.hpi.uni-potsdam.de>
2022-06-25 14:30:40 +0200

    implement basic word count

--- py_classes/statistics_printer.py
+++ py_classes/statistics_printer.py
@@ -1,10 +1,13 @@
+import emoji
 from collections import Counter
 from py_classes.models.MessageTypes import MessageTypes
+from sklearn.feature_extraction.text import CountVectorizer
 
 class StatisticsPrinter():
     def print_statistics(self, preprocessed_chat, emojiArray, print_emojis) : 
         self.print_message_type_statistics(preprocessed_chat)
         self.print_emoji_statistics(emojiArray, print_emojis)
+        self.print_word_count(preprocessed_chat)
 
     def print_message_type_statistics(self, preprocessed_chat) :
         print(" --- MESSAGE TYPE STATISTICS:")
@@ -25,3 +28,23 @@ class StatisticsPrinter():
         for i, item in enumerate(emojiCounter) :
             print(("{:<8} {:>5}").format("    " + item[0], item[1]))
             if print_emojis != "all" and i == 10 : break 
+
+    def print_word_count(self, preprocessed_chat):
+        ## Source: https://stackoverflow.com/questions/33404752/removing-emojis-from-a-string-in-python (modified)
+        def give_emoji_free_text(text):
+            return emoji.get_emoji_regexp().sub(r'', text)
+
+        messages_as_text = ""
+        for message in preprocessed_chat:
+            if message.message_type == MessageTypes.TEXT :
+                message_text = give_emoji_free_text(message.message)
+                for char in '-.,?\n':
+                    message_text=message_text.replace(char,' ').lower()
+                messages_as_text += message_text + " "
+
+        wordsCounter = Counter(messages_as_text.split()).most_common()
+
+        print(("{:<13} {:>10}").format('    WORD','       NUMBER'))
+        for i, item in enumerate(wordsCounter) :
+            print(("{:<15} {:>10}").format("    " + item[0], item[1]))
+            if i == 20 : break 
