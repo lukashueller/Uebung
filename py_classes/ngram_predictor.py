952569a16c5da03eddbfbc05126e27dc0b7fac65
Tim Kuffner <tim.kuffner@me.com>
2022-09-02 16:23:41 +0200

    change predictor to only show the next possible token

--- py_classes/ngram_predictor.py
+++ py_classes/ngram_predictor.py
@@ -15,15 +15,18 @@ class NGRAM_Predictor:
     vectorizer = None
 
     def __init__(self, chat_data):
-        docs = [message.message for message in chat_data if message.message_type == MessageTypes.TEXT]
+        docs = [
+            message.message for message in chat_data if message.message_type == MessageTypes.TEXT]
         self.vectorizer = CountVectorizer(analyzer='word', tokenizer=TweetTokenizer().tokenize, ngram_range=(1, self.NGRAM_MAX), max_df=1.0, min_df=0.0,
-                                     max_features=None)
+                                          max_features=None)
 
-        counts = self.vectorizer.fit_transform(docs)  # counts = document term matrix
+        counts = self.vectorizer.fit_transform(
+            docs)  # counts = document term matrix
 
         self.features = self.vectorizer.get_feature_names_out()
         self.sum_vec = counts.sum(axis=0).tolist()[0]
-        self.feature_matrix = [(f, self.sum_vec[i]) for i, f in enumerate(self.features)] # This is already sorted
+        self.feature_matrix = [(f, self.sum_vec[i]) for i, f in enumerate(
+            self.features)]  # This is already sorted
 
     def preprocess_input(self, text):
         tokenizer = self.vectorizer.build_tokenizer()
@@ -40,13 +43,15 @@ class NGRAM_Predictor:
         results = []
 
         for ngram_length in range(len(preprocessed_input), 0, -1):
-            sss = " ".join(preprocessed_input) + ("" if can_complete_word else " ")
+            sss = " ".join(preprocessed_input) + \
+                ("" if can_complete_word else " ")
             sse = sss[:-1] + chr(ord(sss[-1])+1)
             start_index = bisect_left(self.features, sss)
             end_index = bisect_left(self.features, sse)
 
             result = [
-                (self.sum_vec[i], self.features[i][len(sss):] if len(sss) > 0 else " " + self.features[i], ngram_length)
+                (self.sum_vec[i], self.features[i][len(sss):] if len(
+                    sss) > 0 else " " + self.features[i], ngram_length)
                 for i
                 in range(start_index, end_index+1)
             ]
@@ -55,11 +60,12 @@ class NGRAM_Predictor:
                 res
                 for res
                 in result
-                if res[1].count(" ") <= ngram_length and res[1] != sss and res[1] != ""
+                if res[1][1:].count(" ") < 1 and res[1] != sss and res[1] != ""
             ]
 
             result = sorted(result, key=lambda x: -x[0])
-            result = filter(lambda x: not x[1] in [res[1] for res in results], result)
+            result = filter(lambda x: not x[1] in [res[1]
+                            for res in results], result)
             results += result
 
             preprocessed_input.pop(0)
